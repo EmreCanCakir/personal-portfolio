125cc8f86a331a5d3bbfeb66264e9be03ac1482f
Emre Can Cakir <emrecan.cakir@piworks.net>
2023-05-01 21:05:25 +0300

    add profile page content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'personal-portfolio';
+  circles = [
+    { color: '#123456', size: 100, top: Math.random() * 100, left: Math.random() * 100 },
+    { color: '#789012', size: 150, top: Math.random() * 100, left: Math.random() * 100 },
+    { color: '#345678', size: 75, top: Math.random() * 100, left: Math.random() * 100 }
+  ];
 }
